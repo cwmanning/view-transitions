031dc6ff20899e62eafe39e1efda9ac773c99d32
cwmanning <chris.manning@viget.com>
2023-09-14 16:17:24 -0400

    WIP: remove unused code. add more assets, slides

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,13 @@ import { fileURLToPath } from 'url';
 
 import { defineConfig } from 'astro/config';
 
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
-import icon from 'astro-icon';
-import tasks from './src/utils/tasks';
 
-import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
-
-import { ANALYTICS, SITE } from './src/utils/config.ts';
+import { SITE } from './src/utils/config.ts';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const whenExternalScripts = (items = []) =>
-  ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown
-    ? Array.isArray(items)
-      ? items.map((item) => item())
-      : [items()]
-    : [];
 
 export default defineConfig({
   site: SITE.site,
@@ -35,32 +22,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    sitemap(),
     mdx(),
-    icon({
-      include: {
-        tabler: ['*'],
-        'flat-color-icons': [
-          'template',
-          'gallery',
-          'approval',
-          'document',
-          'advertising',
-          'currency-exchange',
-          'voice-presentation',
-          'business-contact',
-          'database',
-        ],
-      },
-    }),
-
-    ...whenExternalScripts(() =>
-      partytown({
-        config: { forward: ['dataLayer.push'] },
-      })
-    ),
-
-    tasks(),
 
     compress({
       CSS: true,
@@ -74,9 +36,7 @@ export default defineConfig({
     }),
   ],
 
-  markdown: {
-    // remarkPlugins: [readingTimeRemarkPlugin],
-  },
+  markdown: {},
 
   vite: {
     resolve: {
